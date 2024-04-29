14488988806cac0f7093ce897f37cc0558af68f1
SilenceDias <11dias02@mail.ru>
2024-04-29 22:28:27 +0600

    merged two branches

--- StocksApp/Modules/SearchPage/ViewModel/SearchViewModel.swift
+++ StocksApp/Modules/SearchPage/ViewModel/SearchViewModel.swift
@@ -8,12 +8,12 @@
 import Foundation
 
 class SearchViewModel {
-    private let stockManager = StockManager.shared
+    private let stockSearchManager = StockSearchManager.shared
     private var searchResults: [Stock] = []
     
     func search(query: String, completion: @escaping ([Stock]) -> Void) {
         let target = StockTarget.symbolLookup(query: query)
-        stockManager.perform(target) { (result: Result<[Stock], APINetworkError>) in
+        stockSearchManager.perform(target) { (result: Result<[Stock], APINetworkError>) in
             switch result {
             case .success(let stocks):
                 self.searchResults = stocks
