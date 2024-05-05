cab2cd76c4ab8fab38fe7cb34c8da4ffc20f2d60
zhakenovaar <118360824+zhakenovaar@users.noreply.github.com>
2024-05-05 09:09:51 +0500

    added image folder

--- StocksApp/Core/Data/GlobalData/GlobalConstants.swift
+++ StocksApp/Core/Data/GlobalData/GlobalConstants.swift
@@ -12,6 +12,6 @@ enum GlobalConstants {
     static let apiKey = "cojd5v1r01qvn9vpv8c0cojd5v1r01qvn9vpv8cg"
     static let baseAlphaURL = "https://www.alphavantage.co/query"
     static let baseFMPURL = "https://financialmodelingprep.com/api/v3"
-    static let apiKeyAlpha = "WV2X69KCFZSOZNB0"
-    static let apiKeyFMP = "UJsU2sC6wOzaQbcqhYcakZTPvpEoZQnd"
+    static let apiKeyAlpha = "NH3YVJ8B3F87G06X"
+    static let apiKeyFMP = "fngygHiGDyhE6lNjxeEcNX8UI2nuT0Xa"
 }
